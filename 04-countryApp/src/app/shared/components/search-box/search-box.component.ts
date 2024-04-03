c1c2d5ea37a66e745c664f82649ea4a1ee8b96db
unknown <alexraab08@gmail.com>
2024-04-03 17:00:19 -0500

    Country App Fin sección 2 y mejoras

--- 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,20 +1,52 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent {
-
-@Input()
-public placeholder : string = ''
-
-@Output()
-public onValue = new EventEmitter<string>();
+export class SearchBoxComponent implements OnInit, OnDestroy {
 
 
-emitValue(value :string){
-  this.onValue.emit(value);
-}
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
+
+  @Input()
+  public placeholder: string = ''
+
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  @Input()
+  public initialValue : string = ''
+
+  ngOnInit(): void {
+    this.debouncerSubscription = this.debouncer.
+    pipe(
+      debounceTime(1000)
+    ).
+    subscribe( value => {
+      this.onDebounce.emit(value);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe()
+  }
+
+
+  emitValue(value: string) {
+    this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm: string) {
+    this.debouncer.next(searchTerm);
+  }
+
+
+
 }
