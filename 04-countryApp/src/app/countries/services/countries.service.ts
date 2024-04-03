7e35fb4e4b3dab5f03e4f8ceb645c116938be4fb
Alex Ramirez <alexraab08@gmail.com>
2024-04-02 23:17:23 -0500

    Country App

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -3,43 +3,55 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class CountriesService {
 
-  public apiUrl : string = 'https://restcountries.com/v3.1'
+  public apiUrl: string = 'https://restcountries.com/v3.1'
 
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  searchCapital(term : string) : Observable<Country[]> {
-    const url = `${this.apiUrl}/capital/${term}`;
+  searchCountryByAlphaCopde(code: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/alpha/${code}`;
     return this.http.get<Country[]>(url).pipe(
       catchError(error => {
-       console.log(error);
-       return  of([])
+        console.log(error);
+        return of([])
       }
       )
     );
   }
 
-  searchCountry (term : string) : Observable<Country[]>{
+
+  searchCapital(term: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/capital/${term}`;
+    return this.http.get<Country[]>(url).pipe(
+      catchError(error => {
+        console.log(error);
+        return of([])
+      }
+      )
+    );
+  }
+
+  searchCountry(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${term}`;
     return this.http.get<Country[]>(url).pipe(
       catchError(error => {
         console.log(error);
         return of([])
       })
-      )
+    )
   }
 
-  searchRegion (region : string) : Observable<Country[]>{
+  searchRegion(region: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${region}`;
     return this.http.get<Country[]>(url).pipe(
       catchError(error => {
         console.log(error);
         return of([])
       })
-      )
+    )
   }
 
 }
