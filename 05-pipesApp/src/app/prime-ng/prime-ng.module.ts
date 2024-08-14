961213ffecb2b341862579e0e2413219050cf5dc
unknown <alexraab08@gmail.com>
2024-08-14 08:55:46 -0500

    Retoma del proyecto

---
+++ 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MenuModule } from 'primeng/menu';
+
+
+@NgModule({
+  declarations: [],
+  exports : [
+    MenuModule,
+  ],
+  imports: []
+})
+export class PrimeNgModule { }
