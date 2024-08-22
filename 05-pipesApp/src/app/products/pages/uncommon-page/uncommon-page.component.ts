4a2ded569acfef869e010c5c811a1f232646ae8b
unknown <alexraab08@gmail.com>
2024-08-22 17:03:06 -0500

    Pipes no comunes

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  //i18nSelect
+  public name: string = 'Fernando';
+  public gender: 'male' | 'female' = 'male' ;
+
+public invitationMap = {
+  'male': 'invitarlo',
+  'female': 'invitarla'
+}
+
+  changeClient():void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+
+
 }
