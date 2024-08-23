6316f259887fb6720f8a221e0a8748644e849e68
unknown <alexraab08@gmail.com>
2024-08-23 14:44:58 -0500

    Fin seccion Pipes Bássicos

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -9,18 +10,47 @@ export class UncommonPageComponent {
 
   //i18nSelect
   public name: string = 'Fernando';
-  public gender: 'male' | 'female' = 'male' ;
+  public gender: 'male' | 'female' = 'male';
 
-public invitationMap = {
-  'male': 'invitarlo',
-  'female': 'invitarla'
-}
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
 
-  changeClient():void {
+  changeClient(): void {
     this.name = 'Melissa';
     this.gender = 'female';
   }
 
 
+  //i18nPlural
+  public clients: string[] = ['Maria', 'Pedro', 'Fernando', 'Eduardo', 'Melissa', 'Natalia'];
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  public clientsMap = {
+    '=0' : 'no tenemos ninguún cliente esperando.',
+    '=1' : 'tenemos 1 cliente esperando.',
+    '=2' : 'tenemos 2 clientes esperando.',
+    'other' : 'tenemos # clientes esperando.'
+  }
+
+  //KeyValuePipe
+  public person = {
+    name: 'Alex',
+    age: 24,
+    address : 'Malaga 2'
+  }
+
+  //AsyncPipe
+  public myObservableTimer = interval(2000);
+
+  public promisseValue : Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.');
+    }, 3500)
+  })
 
 }
