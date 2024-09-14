6440df8c540313864fd073d95fab355ebebde06c
unknown <alexraab08@gmail.com>
2024-09-13 23:59:07 -0500

    Componentes y consumo base

--- 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroeService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styles: ``
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit{
+
+
+  constructor( private heroService : HeroeService,
+              private activatedRoute : ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id}) => this.heroService.getHeroById(id))
+    ).subscribe(hero => {
+      if(!hero) return this.router.navigate(['/heroes.list'])
+    })
+  }
 
 }
