f2c73f17f1b3ed79602726b8cc1610733e00b923
unknown <alexraab08@gmail.com>
2024-09-16 17:22:11 -0500

    Pagina buscador del heroe y su consumo de informacion

--- 06-heroesApp/src/app/heroes/pages/search-page/search-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroeService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-search-page',
@@ -10,7 +12,20 @@ export class SearchPageComponent {
 
   public searchInput = new FormControl('');
 
+  public heroes : Hero[]= [];
 
+  constructor(private heroService : HeroeService){
+
+  }
+
+  searchHero(){
+    const value : string = this.searchInput.value || '';
+
+    this.heroService.getSuggestions(value)
+     .subscribe( heroes => this.heroes = heroes)
+
+
+  }
 
 
 
