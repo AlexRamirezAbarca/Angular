1e42dbcf2da892bcdf99d80fdad53c605753b415
unknown <alexraab08@gmail.com>
2024-09-24 17:26:13 -0500

    CRUD services

--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -14,7 +14,7 @@ export class HeroeService {
   constructor(private http: HttpClient) { }
 
   getHeroes():Observable<Hero[]> {
-    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`)
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
   }
 
   getHeroById(id : string) : Observable<Hero | undefined>{
@@ -27,6 +27,26 @@ export class HeroeService {
   getSuggestions(query : string) : Observable<Hero[]>{
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${ query }&_limit=6`);
   }
+
+  addHero (hero : Hero) : Observable<Hero>{
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`,hero);
+  }
+
+  updateHero (hero : Hero) : Observable<Hero>{
+    if(!hero.id) throw Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${ hero.id }`,hero);
+  }
+
+  deleteHeroById (id : string) : Observable<boolean>{
+
+    return this.http.delete<Hero>(`${this.baseUrl}/heroes/${ id }`)
+    .pipe(
+      catchError (err => of(false)),
+      map(resp => true)
+    );
+  }
+
 
 
 
