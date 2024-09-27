8890e0f879316b52bbb88346d3320053df6c16f2
unknown <alexraab08@gmail.com>
2024-09-27 11:20:58 -0500

    CRUD finalizado

--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -42,8 +42,9 @@ export class HeroeService {
 
     return this.http.delete<Hero>(`${this.baseUrl}/heroes/${ id }`)
     .pipe(
+      map(resp => true) ,
       catchError (err => of(false)),
-      map(resp => true)
+
     );
   }
 
