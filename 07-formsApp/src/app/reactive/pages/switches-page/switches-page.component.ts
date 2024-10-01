4ed89a827725c2706a34aec35becbc3486de9886
unknown <alexraab08@gmail.com>
2024-10-01 17:10:25 -0500

    Switched validationes

--- 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { resolve } from 'node:path';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
 export class SwitchesPageComponent {
+
+  public myForm : FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  })
+
+  constructor(private fb : FormBuilder){}
+
+  onSave() {
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
 
 }
